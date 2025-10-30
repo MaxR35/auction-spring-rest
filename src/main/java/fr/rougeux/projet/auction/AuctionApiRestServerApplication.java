3e09c75a1cae4ae5692e97a41ec636a9239275cc
Rougeux Max <rougeux.max@gmail.com>
2025-10-30 12:44:41 +0100

    remain untracked files

---
+++ src/main/java/fr/rougeux/projet/auction/AuctionApiRestServerApplication.java
@@ -0,0 +1,13 @@
+package fr.rougeux.projet.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionApiRestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionApiRestServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/rougeux/projet/auction/AuctionApiRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rougeux.projet.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApiRestServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
